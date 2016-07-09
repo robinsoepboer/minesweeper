93a3d58ccabdb1851736de090a74e2d664a06986
robinsoepboer <robinsoepboer@outlook.com>
2016-07-09 16:40:15 +1000

    Fixed bug in right click directive

--- app/directives/ms-right-click/ms-right-click.directive.js
+++ app/directives/ms-right-click/ms-right-click.directive.js
@@ -7,7 +7,7 @@
     
     function msRightClick($parse){
         return function(scope, element, attrs) {
-            var fn = $parse(attrs.ngRightClick);
+            var fn = $parse(attrs.msRightClick);
             element.bind('contextmenu', function(event) {
                 scope.$apply(function() {
                     event.preventDefault();
